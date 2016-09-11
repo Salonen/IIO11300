d315d9f54e7e6a36efb5fd4d56aca91e6526d430
Salonen <simosamulisalonen@gmail.com>
2016-09-11 19:57:29 +0300

    Tehtävä muuten valmis, mutta en saanut tuota sydäntä vasempaan yläkulmaan.

---
+++ IIO11300Vktehtavat/Tehtava2/BLLotto.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    class Lotto
+    {
+        public static string Suomi(int drawns)
+        {
+            //comboBox.Items.Add("Sunday");
+            //comboBox.Items.Add("Monday");
+            Random rnd = new Random();
+            int numero,n;
+            //taul = new int[7];
+            int[] taul = new int[7];
+            string palautus="";
+
+            for (int m = 0; m < drawns; m++)
+            {
+                for (n = 0; n < 7; n++)
+                {
+                    numero = rnd.Next(1, 39+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    taul[n] = numero;
+                }
+                for (n = 0; n < 7; n++)
+                {
+                    numero = rnd.Next(1, 39+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    palautus += " " + numero.ToString();
+                }
+                palautus += "\n";
+            }
+
+            return palautus;
+        }
+
+        public static string Viking(int drawns)
+        {
+            //comboBox.Items.Add("Sunday");
+            //comboBox.Items.Add("Monday");
+            Random rnd = new Random();
+            int numero, n;
+            //taul = new int[7];
+            int[] taul = new int[6];
+            string palautus = "";
+
+            for (int m = 0; m < drawns; m++)
+            {
+                for (n = 0; n < 6; n++)
+                {
+                    numero = rnd.Next(1, 48+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    taul[n] = numero;
+                }
+                for (n = 0; n < 6; n++)
+                {
+                    numero = rnd.Next(1, 48+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    palautus += " " + numero.ToString();
+                }
+                palautus += "\n";
+            }
+
+            return palautus;
+        }
+
+        public static string Euro(int drawns)
+        {
+            //comboBox.Items.Add("Sunday");
+            //comboBox.Items.Add("Monday");
+            Random rnd = new Random();
+            int numero, n;
+            //taul = new int[7];
+            int[] taul = new int[5];
+            string palautus = "";
+
+            for (int m = 0; m < drawns; m++)
+            {
+                for (n = 0; n < 5; n++)
+                {
+                    numero = rnd.Next(1, 50+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    taul[n] = numero;
+                }
+                for (n = 0; n < 5; n++)
+                {
+                    numero = rnd.Next(1, 50+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    palautus += " " + numero.ToString();
+                }
+                palautus += "  ";
+                for (n = 0; n < 2; n++)
+                {
+                    numero = rnd.Next(1, 8+1); // creates a number between 1 and 12
+                                              //numero = 10;
+                    palautus += " " + numero.ToString();
+                }
+                palautus += "\n";
+            }
+
+            
+
+            return palautus;
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            comboBox.Items.Add("Suomi");
+            comboBox.Items.Add("VikingLotto");
+            comboBox.Items.Add("EuroJackpot");
+            
+            
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Class2.joo();
+             try
+            {
+                //if (comboBox.SelectedItem == "Suomi") ;
+                /*switch (comboBox.SelectedItem)
+                {
+                    case "Suomi": break;
+                    case "VikingLotto": break;
+                }*/
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //yield to an user that everything okay
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //Class2.joo();
+           
+            
+            string tulos = "";
+            int drawns = int.Parse(textBox.Text);
+
+            try
+            {
+
+
+                if (comboBox.SelectedItem == "Suomi") {
+                    tulos=Lotto.Suomi(drawns);
+                }
+                else if (comboBox.SelectedItem == "VikingLotto")
+                {
+                    tulos = Lotto.Viking(drawns);
+                }
+
+                else if (comboBox.SelectedItem == "EuroJackpot")
+                {
+                    tulos = Lotto.Euro(drawns);
+                }
+                /*switch (comboBox.SelectedItem)
+                {
+                    case "Suomi": break;
+                    case "VikingLotto": break;
+                }*/
+                textBlock2.Text = tulos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //yield to an user that everything okay
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                textBlock2.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //yield to an user that everything okay
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Environment.Exit(1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //yield to an user that everything okay
+            }
+            
+        }
+    }
+}
+
+/*public class BusinessLogicWindow
+{
+    /// <summary>
+    /// CalculatePerimeter calculates the perimeter of a window
+    /// </summary>
+    public static void Suomi()
+    {
+        //throw new System.NotImplementedException();
+
+    }
+    public static void Viking()
+    {
+        //throw new System.NotImplementedException();
+      
+    }
+    public static void Euro()
+    {
+      
+    }
+}*/
+
+
+
+/*namespace Tehtava2
+{
+    class Class2
+    {
+        public static void joo()
+        {
+            comboBox.Items.Add("Sunday");
+            comboBox.Items.Add("Monday");
+        }
+    }
+}*/
