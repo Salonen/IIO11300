903f45edc13acb2d996ed8150d6b45745d556cdf
h8482 <h8482@ICT-422-WS-06>
2016-10-11 16:10:51 +0300

    MySql toimii jo aika kivasti.

--- IIO11300Vktehtavat/Kiekko/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Kiekko/MainWindow.xaml.cs
@@ -311,211 +311,163 @@ namespace Kiekko
 
         private void button8_Click(object sender, RoutedEventArgs e)
         {
-            /*string connectionString = @"Provider=Microsoft Office 12.0 Access Database Engine OLE DB Provider;" + @"Data source= C:\Users\nearod\Desktop\ImportDB.accdb";
-
-            string queryString = "SELECT * FROM [SQL Agent Unique ID Test Load]";
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed to connect to data source");
-            }*/
+           
         }
 
         private void button9_Click(object sender, RoutedEventArgs e)
         {
-            
+            string cs = "server=mysql.labranet.jamk.fi;user=H8482;" + "pwd=HS1gRvSg2Kv4YCYChteSNOD7pBfSNPh1;database=H8482;"; //@
 
+            MySqlConnection conn = null;
+            MySqlDataReader rdr = null;
 
-
-
-            /*using (var conn = new OleDbConnection(@"Provider=Pelaajat;Data Source=G:\c#2\IIO11300\SMLiiga.accdb")) //Microsoft.ACE.OLEDB.12.0
-            using (var comm = conn.CreateCommand())
+            try
             {
-                comm.CommandText = "SELECT * FROM etunimi";
-                comm.CommandType = CommandType.Text;
+                conn = new MySqlConnection(cs);
+                //conn.Open();
 
+                //string stm = "INSERT INTO pelaajat (etunimi, sukunimi, siirtohinta, seura ) VALUES(@a, @b, @10, @c)";
+
+                //string stm = "INSERT INTO MyGuests (firstname, lastname, email) VALUES('John', 'Doe', 'john@example.com')";
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = String.Format ("INSERT INTO pelaajat (etunimi, sukunimi, siirtohinta, seura ) VALUES('{0}', '{1}', '{2}', '{3}')", pelaajat[0].Etunimi, pelaajat[0].Sukunimi, pelaajat[0].Siirtohinta, pelaajat[0].Seura);//"INSERT INTO tb_mitarbeiter (Vorname) VALUES ('tom')";
+                // "The original string: '{0}'", original
                 conn.Open();
+                command.ExecuteNonQuery();
 
-                var returnValue = comm.ExecuteScalar();
 
-                MessageBox.Show(returnValue.ToString());
-            }*/
 
-            //OleDbConnection connectionString = new OleDbConnection(@"Provider=Pelaajat;" + @"Data Source=G:\c#2\IIO11300\SMLiiga.accdb");
+//                MySqlCommand cmd = new MySqlCommand(stm, conn);
 
-            /*string connectionString = @"Provider=Microsoft.ACE.OLEDB.14.0;" + @"Data Source=G:\c#2\IIO11300\SMLiiga.accdb";
+               
+                //conn.Open();
 
-            string queryString = "SELECT * FROM etunimi";
+                /*DataTable dt2 = new DataTable();
 
-            using (OleDbConnection connection = new OleDbConnection(connectionString))
-            {
-                OleDbCommand command = new OleDbCommand(queryString, connection);
-                connection.Open();
-                OleDbDataReader reader = command.ExecuteReader();
 
-                while (reader.Read())
+
+                using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
                 {
-                    /*string companyCode = reader.GetValue(0).ToString();
-                    string agentId = reader.GetString(1);
-                    string firstName = reader.GetString(2);
-                    string lastName = reader.GetString(3);
-                    string nameSuffix = reader.GetString(4);
-                    string corporateName = reader.GetString(5);
-                    string entityType = reader.GetString(6);
-                    string obfSSN = reader.GetString(7);
-                    string obfFEIN = reader.GetString(8);
-                    string dummyIndicator = reader.GetString(9);*/
-            // Insert code to process data.
-            /*      listBox.ItemsSource = reader.GetString(1);
-              }
-              reader.Close();
-          }
-
-
-
-
-          /*object[] meta = new object[10];
-          bool read;
-          using (con)
-              /*Microsoft.ACE.OLEDB.12.0 */
-            /*{
-                //con.Open();
-                string loo = /*"ServerVersion: {0} \nDataSource: {1}"+*/ //con.ToString();// + con.DataSource;// + con.DataSource;
-
-            /*OleDbCommand command = new OleDbCommand("select * from Pelaajat", con);
-
-            //con.Open();
-            OleDbDataReader reader = command.ExecuteReader();
-
-            if (reader.Read() == true)
-            {
-                do
-                {
-                    int NumberOfColums = reader.GetValues(meta);
-
-                    for (int i = 0; i < NumberOfColums; i++)
-                        listBox.ItemsSource = meta[i].ToString();
-
-                    //Console.WriteLine();
-                    read = reader.Read();
-                } while (read == true);
-            }
-            reader.Close();*/
-            /*string queryString = "SELECT OrderID, CustomerID FROM Orders";
-
-                OracleCommand command = new OracleCommand(queryString, con);
-                con.Open();
-                OracleDataReader reader;
-                reader = command.ExecuteReader();
-
-                // Always call Read before accessing data.
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader.GetInt32(0) + ", " + reader.GetString(1));
+                    da.Fill(dt2);
                 }
 
-                // Always call Close when done reading.
-                reader.Close();*/
+                textBox.Text = dt2.Rows[0].ToString();
 
-            //   }
-            // ToString() !!!!!!
+                int i = 0;
+                string seura;
+                while (i < dt2.Rows.Count)
+                {
+                    seura = dt2.Rows[i]["seura"].ToString();
 
+                    textBox.Text = dt2.Rows[i]["etunimi"].ToString();
+                    textBox1.Text = dt2.Rows[i]["sukunimi"].ToString();
+                    textBox2.Text = dt2.Rows[i++]["siirtohinta"].ToString();
+                    comboBox.Items.Add(seura);
+                    pelaajat.Add(new Pelaaja(textBox.Text, textBox1.Text, seura, int.Parse(textBox2.Text)));
 
-            /*string sql = "SELECT * FROM vCustomers";
-            SqlDataAdapter da = new SqlDataAdapter(sql, con);
-            dt.Clear();
+                }*/
 
-            da.Fill(dt);
+                conn.Close();
 
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
 
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
 
+                if (conn != null)
+                {
+                    conn.Close();
+                }
 
-           // dataGrid.DataContext = dt;
-
-            listBox.DisplayMemberPath = "Lastname";
-
-            listBox.ItemsSource = dt.DefaultView;*/
-
-            //listBox.ItemsSource = loo;
+            }
 
 
         }
 
         private void button10_Click(object sender, RoutedEventArgs e)
         {
-            /*CREATE TABLE oma(
-            firstname VARCHAR(30) NOT NULL,
-            lastname VARCHAR(30) NOT NULL,
-            seura VARCHAR(30) NOT NULL,
-            hinta INT(6) NOT NULL
-            )*/
+            string cs = "server=mysql.labranet.jamk.fi;user=H8482;" + "pwd=HS1gRvSg2Kv4YCYChteSNOD7pBfSNPh1;database=H8482;"; //@
 
-            string connStr = "server=mysql.labranet.jamk.fi;user=H8482;password=joo;";//"server=localhost;user=root;port=3306;password=mysql;"
-                                                              // Älä laita salasanaa Githubbiin
-            /*MySqlConnection conn = new MySqlConnection(connStr);
-            MySqlCommand cmd;
-            string s0;
-            // autentikoi*/
+            MySqlConnection conn = null;
+            MySqlDataReader rdr = null;
+
             try
             {
-                /*using (var conn = new MySqlConnection(connStr))
-                using (var cmd = conn.CreateCommand())
+                conn = new MySqlConnection(cs);
+                conn.Open();
+
+                string stm = "SELECT * FROM pelaajat";
+
+                MySqlCommand  cmd = new MySqlCommand(stm, conn);
+
+                //conn.Open();
+
+                DataTable dt2 = new DataTable();
+
+
+
+                using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
                 {
-                    conn.Open();
-                    cmd.CommandText = "SELECT * ALL pelitietokoneet;";
-                    cmd.ExecuteNonQuery();
-                    conn.Close();
-                }*/
-                //string connection = "Server=localhost;Database=user_table;Uid=root;Pwd=";
-                MySqlConnection dbcon = new MySqlConnection(connStr);
-                DataTable dt = new DataTable();
+                    da.Fill(dt2);
+                }
 
-                MySqlCommand cmd;
-                dbcon.Open();
+                 textBox.Text= dt2.Rows[0].ToString();
+                
+                int i = 0;
+                string seura;
+                while (i < dt2.Rows.Count )
+                {
+                    seura = dt2.Rows[i]["seura"].ToString();
 
-                cmd = dbcon.CreateCommand();
-                cmd.CommandText = "SELECT * FROM henkilot2";
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                adapter.Fill(dt);
-                /*dataGrid1.DataSource = dt;
-                da,taGrid1.DataBind();*/
+                    textBox.Text = dt2.Rows[i]["etunimi"].ToString();
+                    textBox1.Text = dt2.Rows[i]["sukunimi"].ToString();
+                    textBox2.Text = dt2.Rows[i++]["siirtohinta"].ToString();
+                    comboBox.Items.Add(seura);
+                    pelaajat.Add(new Pelaaja(textBox.Text, textBox1.Text, seura, int.Parse(textBox2.Text)));
+                
+                }
 
-                dataGrid1.AutoGenerateColumns = true;
-                dataGrid1.ItemsSource = dt.DefaultView;
+                conn.Close();
 
-
-
-
-                /* conn.Open();
-
-                 s0 = "CREATE DATABASE IF NOT EXISTS `simo`;";
-                 cmd = new MySqlCommand(s0, conn);
-                 cmd.ExecuteNonQuery();
-
-                 //conn.Close();
-                 //s0 = "CREATE DATABASE IF NOT EXISTS simo;"; // simo'simo`
-                 s0 = "CREATE TABLE oma(firstname VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL,seura VARCHAR(30) NOT NULL, hinta INT(6) NOT NULL) ";
-                 cmd = new MySqlCommand(s0, conn);
-                 cmd.ExecuteNonQuery(); // HUOM TÄRKEÄ*/
-
-
-
-                //conn.Close();
             }
-            catch (Exception ex)
+            catch (MySqlException ex)
             {
-                MessageBox.Show(ex.Message);
+                Console.WriteLine("Error: {0}", ex.ToString());
+
             }
             finally
             {
-                //yield to an user that everything okay
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+
             }
+
         }
+
     }
 }
+
+
+               
+       
+    
+
 
 
 
